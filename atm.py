c52ac99c086f4fbc09cc41011dc6befb19c25a12
Mohit Deswal <deswalmohit315@gmail.com>
2025-11-24 08:42:27 +0530

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,36 @@
+
+
+# amount=100000
+# pin=1234
+
+# try:
+#  enter_pin=int(input("please enter your pin :")) 
+
+#  while enter_pin==1234:
+#         a=int(input("press 1 to fetch your balance\n"
+#                     "press 2 for withdraw from your current account\n" 
+#                     "press 3 to deposit cash into your account\n"
+#                     "press 4 to exit transaction:"))
+#         if a==1:
+#             print(amount)
+#         elif a==2:
+#             b=int(input("enter amount:"))
+#             if b>amount:
+#                 print("insufficient balance")
+#             elif b<amount:
+#              amount-=b
+#             print("your current balance is:",amount)
+#         elif a==3:
+#              b=int(input("enter amount:"))
+#              amount+=b
+#              print("your current balance is:",amount)
+#         elif a==4:
+#             break
+#         print("------------------")
+#         elif a!=1,2,3,4:
+      
+        
+#  if enter_pin!=1234:
+#      print("please enter valid pin")
+# except:
+#  print("please enter valid value")
